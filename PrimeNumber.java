d07254b5f7e7f7bc33591667ce3e13815fd01aee
Nikhil Satyam <satyamnikhil1523@gmail.com>
2023-01-16 14:58:06 +0530

    update code

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
+
 public class PrimeNumber {
 
     public static void main(String[] args) {
 
-        int num = 29;
+        // int num = 29;
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
         boolean flag = false;
         for(int i = 2; i <= Math.sqrt(num); i++)
         {
@@ -23,6 +27,8 @@ public class PrimeNumber {
             System.out.println(num + " is a prime number.");
         else
             System.out.println(num + " is not a prime number.");
+
+        scanner.close();
     }
 }
 //write a program to find prime numbers in java" 
