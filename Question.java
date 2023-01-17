4c5746a34e3fbd2d6985f1aa1ce344f25b98cae7
Nikhil Satyam <satyamnikhil1523@gmail.com>
2023-01-17 14:43:49 +0530

    push code

---
+++ Question.java
@@ -0,0 +1,18 @@
+class Question {
+    public static void main(String args[]) {
+        // byte b = 10, c = 20, d;
+        // d = (byte) (b + c);
+        // System.out.println(d);
+        // byte k = (byte) 470;
+        // System.out.println(34>>3);
+        // System.out.println(-34>>3);
+        // System.out.println(-34>>>3);
+        int x = 10 , y = 5 ;
+        while(x-- > 7 || ++y < 8){
+            System.out.println(x);
+            System.out.println(y);
+        }
+        System.out.print(x);
+        System.out.print(y);
+    }
+}
