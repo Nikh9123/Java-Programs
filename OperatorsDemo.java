6a129ccbb33afaaf0ddfaf9079f5f6f50e6bef8e
Nikhil Satyam <satyamnikhil1523@gmail.com>
2023-01-17 14:39:59 +0530

    add code

---
+++ OperatorsDemo.java
@@ -0,0 +1,14 @@
+class OperatorsDemo {
+
+    public static void main(String args[]) {
+        // byte b = 10, c = 20, d;
+        // d = (byte) (b + c);
+        // System.out.println(d);
+        // byte k = (byte) 470;
+        System.out.println(34>>3);
+        System.out.println(-34>>3);
+        System.out.println(-34>>>3);
+
+
+    }
+}
