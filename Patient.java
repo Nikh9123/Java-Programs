945730410fa595a3d3ea525c7caea7903d70d177
Nikhil Satyam <satyamnikhil1523@gmail.com>
2023-02-01 14:37:34 +0530

    push code

---
+++ Patient.java
@@ -0,0 +1,22 @@
+// import java.util.concurrent.ThreadPoolExecutor.DiscardOldestPolicy;
+
+// import java.io.*;
+class Patient{
+    String Pname ;
+    int Page ;
+    long PCno ;
+
+    Patient(String Name , int age , int Cno){
+        this.Pname = Name ;
+        this.Page = age ;
+        this.PCno = Cno;
+    }
+    void Display(){
+        System.out.println("pateint name : " + Pname + "\n age : " + Page + " \n con. no :" + PCno);
+
+    }
+    public static void main(String[] rk ) {
+       Patient details = new  Patient("Raj" , 30 , 12100798);
+       details.Display();
+    }
+}
